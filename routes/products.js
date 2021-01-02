48246ca6e2977f3eb6a4c17bbc5956fd4dc783e0
moeed <72702114+moeed1999@users.noreply.github.com>
2021-01-02 17:37:02 +0500

    123

---
+++ routes/products.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var router = express.Router();
+var Product = require("../models/product");
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  let products = await Product.find();
+  console.log(products);
+  res.render("products/list", { title: "Products in DB", products });
+});
+router.get("/add", async function (req, res, next) {
+  res.render("products/add");
+});
+// store data in db
+router.post("/add", async function (req, res, next) {
+  let product = new Product(req.body);
+  await product.save();
+  res.redirect("/products");
+});
+module.exports = router;
